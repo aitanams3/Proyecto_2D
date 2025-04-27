e2a8d59209621aa4b8570644aaf716cbed85e7dc
aitanams3 <aitmarsal@gmail.com>
2025-04-28 00:27:06 +0200

    VIDEO 42

--- Assets/Scripts/FantasmaScript.cs
+++ Assets/Scripts/FantasmaScript.cs
@@ -9,6 +9,8 @@ public class FantasmaScript : MonoBehaviour
     GameObject personaje; 
     public float velocidadFantasma = 1f;
 
+    AudioSource _audioSource;
+
 
     // Start is called before the first frame update
     void Start()
@@ -16,6 +18,7 @@ public class FantasmaScript : MonoBehaviour
         posicionInicial = transform.position;
         //personaje = GameObject.Find("Personaje");
         personaje = GameObject.FindGameObjectWithTag("Player");
+        _audioSource = this.GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -28,10 +31,22 @@ public class FantasmaScript : MonoBehaviour
         if (distancia <= 3f)
         {
             transform.position = Vector3.MoveTowards(transform.position, personaje.transform.position, velocidadFinal);
+
+
+            if(_audioSource.isPlaying == false){
+                _audioSource.Play();
+            }
+
         }
         else
         {
             transform.position = Vector3.MoveTowards(transform.position, posicionInicial, velocidadFinal); 
+
+
+            if((transform.position == posicionInicial) && _audioSource.isPlaying == true){
+                 _audioSource.Stop();
+            }
+
         }
 
         // Debug.DrawLine(transform.position, new Vector3(5, 0, 0), Color.white, 2.5f);
