1933dc63996a1858684e946769203f8509250080
aitanams3 <aitmarsal@gmail.com>
2025-04-27 12:46:11 +0200

    Video 38

--- Assets/Scripts/InicioScript.cs
+++ Assets/Scripts/InicioScript.cs
@@ -5,10 +5,14 @@ using UnityEngine.SceneManagement;
 
 public class InicioScript : MonoBehaviour
 {
+
+    GameObject panelSettings ;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        panelSettings = GameObject.Find("PanelSettings");
+        panelSettings.SetActive(false);
     }
 
     // Update is called once per frame
@@ -24,10 +28,16 @@ public class InicioScript : MonoBehaviour
     }
 
     public void ExitGame(){
-
         Debug.Log("exit");
-
         Application.Quit();
-
     }
+
+    public void MostrarSettings(){
+         panelSettings.SetActive(true);
+    }
+
+    public void OcultarSettings(){
+         panelSettings.SetActive(false);
+    }
+
 }
